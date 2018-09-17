5aec513e4f8af2d596ad56407ea2d7d447402fda
Kathleen Jumamoy <kathleen.jumamoy@myob.com>
2018-09-18 08:25:48 +1200

    refactored

--- HelloWorld/HelloWorld/WebHostBuilder.cs
+++ HelloWorld/HelloWorld/WebHostBuilder.cs
@@ -7,28 +7,29 @@ using Microsoft.AspNetCore.Http;
 
 public class WebHostBuilder
 {
+    //rename function name to Run? Build?
     public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
         WebHost.CreateDefaultBuilder(args)
             .Configure(app =>
             {
                 app.Run(async context =>
                 {
-                    await GetResponse(context);
+                    var requestMethod = context.Request.Method;
+                    await GetResponse(requestMethod, context);
                 });
             });
 
-    private static async Task GetResponse(HttpContext context)
-    {
-        var requestType = context.Request.Method;
-        switch (requestType)
+    private static async Task GetResponse(string requestMethod, HttpContext context)
+    { 
+        switch (requestMethod)
         {
             case "GET":
                 context.Response.StatusCode = 200;
-                await context.Response.WriteAsync($"Hello Kathleen");
+                await context.Response.WriteAsync("Hello Kathleen");
                 break;
             case "POST":
                 context.Response.StatusCode = 200;                
-                await context.Response.WriteAsync($"new user sdkdfghdgh");
+                await context.Response.WriteAsync("new user sdkdfghdgh");
                 break;
         }
     }
